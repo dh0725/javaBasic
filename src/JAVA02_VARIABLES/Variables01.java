81c6a48a1dca7924eca51e90ccea8fe1ba43d3b7
dh0725 <gidion3820@gmail.com>
2022-07-12 16:45:04 +0900

    JAVA02_VARIABLES

---
+++ src/JAVA02_VARIABLES/Variables01.java
@@ -0,0 +1,14 @@
+package JAVA02_VARIABLES;
+
+public class Variables01 {
+    public static void main(String[] args) {
+        System.out.println("=====Boolean=====");
+        boolean isMaried;
+        isMaried = false;
+        System.out.println("isMaried : " + isMaried);
+
+        System.out.println("=====character=====");
+        char gender = 'M';
+        System.out.println("gender : " + gender);
+    }
+}
